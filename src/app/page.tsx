32a691f968e2c312b32c61853bf54e7063703788
jzwoo <jzwoo98@gmail.com>
2023-07-23 14:09:49 +0800

    Added basic login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,91 @@
-import Image from 'next/image'
+"use client"
 
-export default function Home() {
-  return (
-    <h1>
-      This is home
-    </h1>
-  )
+import Link from "next/link"
+import {zodResolver} from "@hookform/resolvers/zod"
+import * as z from "zod"
+
+import {Button} from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import {Input} from "@/components/ui/input"
+import {useForm} from "react-hook-form";
+import React from "react";
+
+const formSchema = z.object({
+    username: z.string().min(2),
+    password: z.string().min(2)
+})
+
+const Login: React.FC = () => {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            password: ""
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+    return (
+        <div className="p-5 w-96 m-auto h-screen">
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5 h-full flex flex-col justify-center">
+                    <FormDescription>
+                        Admin login.
+                    </FormDescription>
+
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        htmlFor="username"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Username</FormLabel>
+
+                                <FormControl>
+                                    <Input placeholder="username" {...field} />
+                                </FormControl>
+
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+
+                                <FormControl>
+                                    <Input placeholder="password" {...field} />
+                                </FormControl>
+
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+
+                    <Button type="submit">LOGIN</Button>
+                </form>
+            </Form>
+        </div>
+    )
 }
+
+export default Login;
