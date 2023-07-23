1119c00cebf9818641b42e69a2da2948408838ce
jzwoo <jzwoo98@gmail.com>
2023-07-23 14:36:31 +0800

    Add Card styling to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,9 @@ import {
 import {Input} from "@/components/ui/input"
 import {useForm} from "react-hook-form";
 import React from "react";
+import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {cn} from "@/lib/utils";
+import {className} from "postcss-selector-parser";
 
 const formSchema = z.object({
     username: z.string().min(2),
@@ -41,49 +44,56 @@ const Login: React.FC = () => {
     }
 
     return (
-        <div className="p-5 w-96 m-auto h-screen">
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5 h-full flex flex-col justify-center">
-                    <FormDescription>
-                        Admin login.
-                    </FormDescription>
+        <div className="flex justify-center items-center h-full">
+            <Card className={cn("w-[380px] h-[fit-content]", className)}>
+                <CardHeader>
+                    <CardTitle>Admin Login</CardTitle>
+                </CardHeader>
 
-                    <FormField
-                        control={form.control}
-                        name="username"
-                        htmlFor="username"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Username</FormLabel>
+                <CardContent>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}
+                              className="space-y-5 h-full flex flex-col justify-center">
+                            <FormField
+                                control={form.control}
+                                name="username"
+                                htmlFor="username"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Username</FormLabel>
 
-                                <FormControl>
-                                    <Input placeholder="username" {...field} />
-                                </FormControl>
+                                        <FormControl>
+                                            <Input placeholder="username" {...field} />
+                                        </FormControl>
 
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
 
-                    <FormField
-                        control={form.control}
-                        name="password"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Password</FormLabel>
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
 
-                                <FormControl>
-                                    <Input placeholder="password" {...field} />
-                                </FormControl>
+                                        <FormControl>
+                                            <Input placeholder="password" {...field} />
+                                        </FormControl>
 
-                                <FormMessage/>
-                            </FormItem>
-                        )}
-                    />
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                        </form>
+                    </Form>
+                </CardContent>
 
-                    <Button type="submit">LOGIN</Button>
-                </form>
-            </Form>
+                <CardFooter>
+                    <Button className="mt-5 w-full" type="submit">LOGIN</Button>
+                </CardFooter>
+            </Card>
         </div>
     )
 }
